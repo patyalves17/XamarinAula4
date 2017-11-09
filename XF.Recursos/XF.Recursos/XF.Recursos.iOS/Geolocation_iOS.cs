da9f5a8caf537fd9325ed4f726223f23b532ebec
patyalves17 <patriciaalves10@gmail.com>
2017-11-08 23:02:09 -0200

    aula 4

---
+++ XF.Recursos/XF.Recursos/XF.Recursos.iOS/Geolocation_iOS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using XF.Recursos.GPS;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Geolocation;
+
+namespace XF.Recursos.iOS
+{
+   public class GeoLocation_iOS : ILocalizacao
+    {
+        public void GetCoordenada()
+        {
+            var locator = new Geolocator { DesiredAccuracy = 50 };
+            locator.GetPositionAsync(timeout: 10000).ContinueWith(t => {
+                SetCoordenada(t.Result.Latitude, t.Result.Longitude);
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        void SetCoordenada(double paramLatitude, double paramLongitude)
+        {
+            var coordenada = new Coordenada()
+            {
+                Latitude = paramLatitude.ToString(),
+                Longitude = paramLongitude.ToString()
+            };
+
+            MessagingCenter.Send<ILocalizacao, Coordenada>(this, "coordenada", coordenada);
+        }
+    }
+}
